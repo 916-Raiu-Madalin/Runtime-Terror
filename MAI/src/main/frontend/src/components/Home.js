2f0b97bf2b1bb26bfd4de12df11e032f566042b2
916-Raiu-Madalin <raiumadalin@yahoo.com>
2022-04-17 21:51:00 +0300

    Small Informative Box on HomePage

--- MAI/src/main/frontend/src/components/Home.js
+++ MAI/src/main/frontend/src/components/Home.js
@@ -1,11 +1,21 @@
-import { Typography, Box } from "@mui/material"
-import {React} from "react"
+import { Typography, Box, Button, CardContent, Card, CardActions} from "@mui/material"
+import {React,useState, useEffect} from "react"
 
+const bull = (
+    <Box
+        component="span"
+        sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+    </Box>
+);
 
 
 
 const Home = ( ) =>{
-
+    const [dateState, setDateState] = useState(new Date());
+    useEffect(() => {
+        setInterval(() => setDateState(new Date()), 30000);
+    }, []);
     return(
         <Box
           sx={{
@@ -13,12 +23,57 @@ const Home = ( ) =>{
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
+            gap: 10,
           }}
         >
         <Typography variant="h3">
             Welcome to MAI
         </Typography>
+        <Card sx={{ minWidth: 500 ,width: '30%',display: 'flex', flexDirection: 'column'}}>
+            <CardContent >
+                <Typography sx={{ fontSize: 24 ,display: 'flex', flexDirection: 'row'}} color="text.secondary" gutterBottom>
+                    Site News
+
+                    <Typography sx={{width:'70%', textAlign:'right', marginTop:'-3%', marginLeft:'6%'}} >
+                        <p>
+                            {dateState.toLocaleString('en-US', {
+                                hour: 'numeric',
+                                minute: 'numeric',
+                                hour12: true,
+                            })}
+                            {' '}
+                            {dateState.toLocaleDateString('en-GB', {
+                                day: 'numeric',
+                                month: 'short',
+                                year: 'numeric',
+                            })}
+                        </p>
+                    </Typography>
+                </Typography>
+                <Typography variant="h5" component="div">
+                    Connection{bull}settings{bull}MAI{bull}
+
+                </Typography>
+
+                <Typography variant="body2" sx={{paddingLeft:5}}>
+                    <b>●The teachers assume their responsibilities for the students that enrol in their course</b>
+                    <br />
+
+                </Typography>
+                <Typography variant="body2" sx={{paddingLeft:5}}>
+                    <b>●The students can login using the username and password that they received on email</b>
+                    <br />
+
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+
+        </Card>
+
         </Box>
+
     )
 }
 
