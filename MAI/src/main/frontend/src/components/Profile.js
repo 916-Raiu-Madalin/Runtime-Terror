bd26f36381eff580f71b04c9539f819c17af5c6a
916-Andrei-Saramet <andreisaramet21@gmail.com>
2022-05-22 19:16:38 +0300

    add enrolment option

--- MAI/src/main/frontend/src/components/Profile.js
+++ MAI/src/main/frontend/src/components/Profile.js
@@ -1,12 +1,14 @@
 import AuthContext from "../context/AuthProvider"
 
 import {React, useContext, useEffect, useState} from "react"
-import {Grid, Paper, Avatar, TextField, Button, Box, Container, FormControlLabel, Typography} from "@mui/material"
+import {
+    Grid, Paper, Avatar, TextField, Button, Box, Container, FormControlLabel, Typography, Alert
+} from "@mui/material"
 import {POST} from "../api/requests"
 import axios from "../api/axios"
 
 
-const Profile=()=>{
+const Profile = () => {
     const {auth, setAuth} = useContext(AuthContext);
     const useMountEffect = (fun) => useEffect(fun, [])
 
@@ -14,106 +16,135 @@ const Profile=()=>{
     const [lastName, setLastName] = useState('')
     const [emailAddress, setEmailAddress] = useState('')
     const [city, setCity] = useState('')
-    
-    
-    const fetchData = (event) =>{
+    const [enrolYear, setEnrolYear] = useState('');
+
+
+    const fetchData = (event) => {
         event?.preventDefault();
         const loggedIn = auth?.logged_in ? true : false
         let username = localStorage.getItem('user');
         let password = localStorage.getItem('password');
         axios({
-            method:'GET',
-            url:'/api/profile',
-            auth:{
-                username:username,
-                password:password
-            },
-            params:{
-                username:username
+            method: 'GET', url: '/api/profile', auth: {
+                username: username, password: password
+            }, params: {
+                username: username
             }
-        }).then(response =>{
+        }).then(response => {
             setFirstName(response.data.firstName)
             setLastName(response.data.lastName)
             setEmailAddress(response.data.emailAddress)
             setCity(response.data.city)
-        }).catch(err=>{
+        }).catch(err => {
             console.log(err)
         })
     }
 
-    const handleSubmit = (event) =>{
+    const handleSubmit = (event) => {
         var Buffer = require('buffer/').Buffer
 
         event.preventDefault();
         const data = new FormData(event.currentTarget)
         data.set('username', localStorage.getItem('user'))
         axios({
-            method: 'post',
-            url: '/api/profile',
-            headers: { 
-                'Authorization': 'Basic '+ Buffer.from(localStorage.getItem('user') + ':' + localStorage.getItem('password')).toString('base64'), 
+            method: 'post', url: '/api/profile', headers: {
+                'Authorization': 'Basic ' + Buffer.from(localStorage.getItem('user') + ':' + localStorage.getItem('password')).toString('base64'),
                 'Content-Type': 'multipart/form-data'
-              },
-            data: data
-                }
-            ).then(response=>
-            console.log(response?.data))
-        .catch(err=>
-            console.log(err))
-        
+            }, data: data
+        }).then(response => console.log(response?.data))
+            .catch(err => console.log(err))
+
     }
+
+    const enrol = (event) => {
+        if (enrolYear < 1 || enrolYear > 3) {
+            setEnrolYear(1);
+            return;
+        }
+        var Buffer = require('buffer/').Buffer
+
+        event.preventDefault();
+        axios({
+            method: 'post', url: '/api/enrol', headers: {
+                'Authorization': 'Basic ' + Buffer.from(localStorage.getItem('user') + ':' + localStorage.getItem('password')).toString('base64'),
+                'Content-Type': 'multipart/form-data'
+            }, params: {
+                year: enrolYear
+            }
+        }).then(response => console.log(response?.data))
+            .catch(err => console.log(err))
+    }
+
     useMountEffect(fetchData);
 
-    return(
-        <Container component="main" maxWidth="xl">
-            <Box onSubmit={handleSubmit} component="form" noValidate>   
+    return (<Container component="main" maxWidth="xl">
+            <Box onSubmit={handleSubmit} component="form" noValidate>
                 <TextField
-                margin="normal"
-                fullWidth
-                id="firstname"
-                label="First Name"
-                name="firstname"
-                value={firstName || ""}
-                onChange={(event) => setFirstName(event.target.value)}
-                autoComplete="firstname"
-                autoFocus
+                    margin="normal"
+                    fullWidth
+                    id="firstname"
+                    label="First Name"
+                    name="firstname"
+                    value={firstName || ""}
+                    onChange={(event) => setFirstName(event.target.value)}
+                    autoComplete="firstname"
+                    autoFocus
                 />
                 <TextField
-                margin="normal"
-                fullWidth
-                id="lastname"
-                label="Last Name"
-                name="lastname"
-                value={lastName || ""}
-                onChange={(event) => setLastName(event.target.value)}
-                autoComplete="lastname"
+                    margin="normal"
+                    fullWidth
+                    id="lastname"
+                    label="Last Name"
+                    name="lastname"
+                    value={lastName || ""}
+                    onChange={(event) => setLastName(event.target.value)}
+                    autoComplete="lastname"
                 />
                 <TextField
-                margin="normal"
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                value={emailAddress || ""}
-                onChange={(event) => setEmailAddress(event.target.value)}
-                autoComplete="email"
+                    margin="normal"
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    value={emailAddress || ""}
+                    onChange={(event) => setEmailAddress(event.target.value)}
+                    autoComplete="email"
                 />
                 <TextField
-                margin="normal"
-                fullWidth
-                id="cith"
-                label="City"
-                name="city"
-                value={city || ""}
-                onChange={(event) => setCity(event.target.value)}
-                autoComplete="city"
+                    margin="normal"
+                    fullWidth
+                    id="cith"
+                    label="City"
+                    name="city"
+                    value={city || ""}
+                    onChange={(event) => setCity(event.target.value)}
+                    autoComplete="city"
                 />
-            <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            >Save</Button>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                >Save</Button>
             </Box>
+            <Box onSubmit={enrol} component="form" noValidate>
+                <TextField
+                    margin="normal"
+                    fullWidth
+                    id="year"
+                    label="Enrolment year"
+                    name="enrol"
+                    value={enrolYear || ""}
+                    onChange={(event) => setEnrolYear(event.target.value)}
+                    autoComplete="1"
+                    autoFocus
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                >Save</Button>
+            </Box>
+
         </Container>
 
     )
