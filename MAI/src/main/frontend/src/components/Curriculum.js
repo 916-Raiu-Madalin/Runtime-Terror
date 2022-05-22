0dfbbf07a9b0355eebff521233f97dfea6953da2
916-Andrei-Saramet <andreisaramet21@gmail.com>
2022-05-22 12:46:00 +0300

    curriculum added year and semester selection

--- MAI/src/main/frontend/src/components/Curriculum.js
+++ MAI/src/main/frontend/src/components/Curriculum.js
@@ -1,10 +1,24 @@
 import axios from "../api/axios";
+import * as React from 'react'
 import TableContainer from "@mui/material/TableContainer";
-import {Paper, Stack, Table, TableBody, TableCell, TableRow} from "@mui/material";
+import {
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Paper,
+    Select,
+    Stack,
+    Table,
+    TableBody,
+    TableCell,
+    TableRow
+} from "@mui/material";
 import TableHead from "@mui/material/TableHead";
 import {useEffect, useState} from "react";
 
 const Curriculum = () => {
+    const [year, setYear] = React.useState('1');
+    const [semester, setSemester] = React.useState('1');
     const [compulsoryDisciplines, setCompulsoryDisciplines] = useState([]);
     const [optionalDisciplines, setOptionalDisciplines] = useState([]);
 
@@ -34,7 +48,10 @@ const Curriculum = () => {
                     username: localStorage.getItem('user'),
                     password: localStorage.getItem('password')
                 },
-                params: {}
+                params: {
+                    year: year,
+                    semester: semester
+                }
             }
         ).then(response => {
             setOptionalDisciplines(response.data);
@@ -43,58 +60,86 @@ const Curriculum = () => {
         });
     }
 
+    function changeYear(event) {
+        setYear(event.target.value);
+    }
+
+    function changeSemester(event) {
+        setSemester(event.target.value);
+    }
+
     useEffect(fetchData, []);
 
     return (
-        <Stack direction="row" spacing={2}>
-            <TableContainer component={Paper}>
-                <Table aria-label="Compulsory Disciplines" title="Compulsory Disciplines">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell colSpan={2} sx={{fontWeight: 'bold'}} align="center">Compulsory
-                                Disciplines</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell sx={{fontWeight: 'bold'}}>Discipline Name</TableCell>
-                            <TableCell sx={{fontWeight: 'bold'}}>Number of ECTS Credits</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {compulsoryDisciplines.map((discipline) => (
-                            <TableRow key={discipline.name}>
-                                <TableCell>{discipline.name}</TableCell>
-                                <TableCell align="center">{discipline.noCredits}</TableCell>
-                            </TableRow>
-                        ))
+        <div>
+            <FormControl sx={{m: 1, minWidth: 80}}>
+                <InputLabel id="select-year-label">Year</InputLabel>
+                <Select labelId="select-year-label" id="year-label" value={year} label="Year" onChange={changeYear}>
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl sx={{m: 1, minWidth: 80}}>
+                <InputLabel id="select-semester-label">Semester</InputLabel>
+                <Select labelId="select-semester-label" id="semester-label" value={semester} label="Semester"
+                        onChange={changeSemester}>
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                </Select>
+            </FormControl>
 
-                        }
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TableContainer component={Paper}>
-                <Table aria-label="Optional Disciplines">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell colSpan={2} sx={{fontWeight: 'bold'}} align="center">Optional
-                                Disciplines</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell sx={{fontWeight: 'bold'}}>Optional Discipline Name</TableCell>
-                            <TableCell sx={{fontWeight: 'bold'}}>Number of ECTS Credits</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {optionalDisciplines.map((discipline) => (
-                            <TableRow key={discipline.name}>
-                                <TableCell>{discipline.name}</TableCell>
-                                <TableCell align="center">{discipline.noCredits}</TableCell>
+
+            <Stack direction="row" spacing={2}>
+                <TableContainer component={Paper}>
+                    <Table aria-label="Compulsory Disciplines" title="Compulsory Disciplines">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell colSpan={2} sx={{fontWeight: 'bold'}} align="center">Compulsory
+                                    Disciplines</TableCell>
                             </TableRow>
-                        ))
-                        }
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </Stack>
+                            <TableRow>
+                                <TableCell sx={{fontWeight: 'bold'}}>Discipline Name</TableCell>
+                                <TableCell sx={{fontWeight: 'bold'}}>Number of ECTS Credits</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {compulsoryDisciplines.map((discipline) => (
+                                <TableRow key={discipline.name}>
+                                    <TableCell>{discipline.name}</TableCell>
+                                    <TableCell align="center">{discipline.noCredits}</TableCell>
+                                </TableRow>
+                            ))
+
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TableContainer component={Paper}>
+                    <Table aria-label="Optional Disciplines">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell colSpan={2} sx={{fontWeight: 'bold'}} align="center">Optional
+                                    Disciplines</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell sx={{fontWeight: 'bold'}}>Optional Discipline Name</TableCell>
+                                <TableCell sx={{fontWeight: 'bold'}}>Number of ECTS Credits</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {optionalDisciplines.map((discipline) => (
+                                <TableRow key={discipline.name}>
+                                    <TableCell>{discipline.name}</TableCell>
+                                    <TableCell align="center">{discipline.noCredits}</TableCell>
+                                </TableRow>
+                            ))
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Stack>
+        </div>
     );
 }
 
